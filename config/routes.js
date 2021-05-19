8be4ad477c2d0de4972862e09fff04e817759147
bgeorgieff <b.g.georgiev87@gmail.com>
2021-05-19 15:30:47 +0300

    fix: fixed 404 loading when entering home

--- config/routes.js
+++ config/routes.js
@@ -1,13 +1,12 @@
 const routes = require('../routes')
 
 module.exports = (app) => {
-	app.use('/home', routes.home)
 	app.use('/', routes.home)   
 	app.use('/user', routes.users)
 	app.use('/createArticle', routes.article)
 	app.use('/viewArticle', routes.article)
 
-	// app.use('*', () => {
-	// 	console.log('this is not the webpage you are looking for')
-	// })
+	app.use('*', () => {
+		console.log('this is not the webpage you are looking for')
+	})
 }
--- handlers/home/index.js
+++ handlers/home/index.js
@@ -3,9 +3,10 @@ const Articles = require('../articles/Article')
 module.exports = {
   get: {
     home(req, res, next) {
-      
-      Articles.find().lean().then((article) => {
 
+      Articles.find().lean().then((article) => {
+        const post = article
+        console.log(post.toString());
         res.render('home.hbs', {
           isLoggedIn: req.user !== undefined,
           article
