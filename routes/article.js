0be5334729623ca86494dff503b7f618a3e229e4
bgeorgieff <b.g.georgiev87@gmail.com>
2021-05-21 12:00:02 +0300

    feat: get edit article req

--- routes/article.js
+++ routes/article.js
@@ -3,7 +3,8 @@ const handler = require('../handlers/articles')
 const isAuth = require('../utils/isAuth')
 
 router.get('/create', isAuth(), handler.get.postArticle)
-router.get('/:id', isAuth(true), handler.get.viewArticle)
+router.get('/post/:id', isAuth(true), handler.get.viewArticle)
+router.get('/edit-post/:id', isAuth(), handler.get.editArticle)
 
 router.post('/create', isAuth(), handler.post.postArticle)
 
